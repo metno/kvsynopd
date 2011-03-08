3e314a1b6ca3e085b64f0ba7f3aed705f2934fb0
Børge Moe <borge.moe@met.no>
2011-03-08 12:03:00 +0000

    Temporary allow "wild obesrevations", ie remove the requirement that useinfo(0)!=9.
    git-svn-id: https://svn.met.no/kvoss/kvsynopd/trunk@2765 422d51e2-5941-41ac-8e65-d5b5931239be

--- src/Validate.cc
+++ src/Validate.cc
@@ -185,13 +185,14 @@ validDataUseOnlyUseInfo( const Data &data )
   int uinfo_3=uinfo.flag( 3 ); //Treatment of original data value.
   int cinfo_mis = cinfo.flag( f_fmis );
 
-  LOGDEBUG("Validate: stationid: " << data.stationID() << " typeid: " << data.typeID()
+ /* LOGDEBUG("Validate: stationid: " << data.stationID() << " typeid: " << data.typeID()
          << " obstime: " << data.obstime()
          << " paramid: " << data.paramID()
          << " original: " << data.original()
          << " cflags: " << data.controlinfo()
-         << " uflags: " << data.useinfo() << " u2: " << uinfo_2 << " u3: " << uinfo_3 << " cmis: "<< cinfo_mis );
+         << " uflags: " << data.useinfo() << " u2: " << uinfo_2 << " u3: " << uinfo_3 << " cmis: "<< cinfo_mis );*/
 
+#if 0
   if( uinfo_0 == 9 ){ //Parameter not defined in obs_pgm.
       IDLOGINFO("uinfo0","REJECTED useinfo(0)=" << uinfo_0 << " :  stationid: " << data.stationID()  << " typeid: " << data.typeID()
                  << " obstime: " << data.obstime()
@@ -199,6 +200,7 @@ validDataUseOnlyUseInfo( const Data &data )
                  << " original: " << data.original());
       return false;
   }
+#endif
 
   if( atoi( data.original().c_str() ) == -32767 ) {
      LOGINFO("REJECTED -32767: stationid: " << data.stationID() << " typeid: " << data.typeID()
