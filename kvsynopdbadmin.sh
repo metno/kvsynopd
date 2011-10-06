cdd5e27593455e1870e046a94e9d837381ba53a3
Børge Moe <borge.moe@met.no>
2011-10-06 09:10:55 +0000

    Add alias check before running cleanups.
    git-svn-id: https://svn.met.no/kvoss/kvsynopd/trunk@3081 422d51e2-5941-41ac-8e65-d5b5931239be

--- kvsynopdbadmin.sh
+++ kvsynopdbadmin.sh
@@ -1,9 +1,23 @@
 #! /bin/sh
 
 SQL=sqlite3
+ETCDIR==`KVCONFIG --sysconfdir`/kvalobs
 SQLDIR=`KVCONFIG --datadir`/kvsynopd/
 LOGDIR=`KVCONFIG --logdir`/kvsynop/
 DBFILE=`KVCONFIG --localstatedir`/lib/kvsynopd/kvsynopd.sqlite
+
+has_alias()
+{
+	aliasname=$1
+	if [ -f /usr/local/sbin/alias_list ]; then
+		/usr/local/sbin/alias_list | /bin/grep -q $aliasname
+		return $?
+	fi
+	return 1
+}
+
+(test -f "$ETCDIR/KVALOBS_TEST") || has_alias kvalobs || die
+
 
 DAY=`date '+%d'`
 LOG=$LOGDIR/kvsynopdb-$DAY.log
